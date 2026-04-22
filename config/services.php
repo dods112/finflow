35d1a3d50b0b20ebd1ea23edfc3fba7f3208fdae
Dods <dodsdolohan2@gmail.com>
2026-04-23 07:47:29 +0800

    fix: groq AI config

--- config/services.php
+++ config/services.php
@@ -2,23 +2,11 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain'   => env('MAILGUN_DOMAIN'),
+        'secret'   => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
+        'scheme'   => 'https',
     ],
 
     'postmark' => [
@@ -26,14 +14,24 @@ return [
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
+        'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-
     'groq' => [
         'key' => env('OPENAI_API_KEY', ''),
+    ],
+
+    'openrouter' => [
+        'key' => env('OPENROUTER_API_KEY', ''),
+    ],
+
+    'finflow_ai' => [
+        'provider'     => env('FINFLOW_AI_PROVIDER', 'groq'),
+        'model'        => env('FINFLOW_AI_MODEL', 'llama-3.1-8b-instant'),
+        'ollama_url'   => env('OLLAMA_URL', 'http://localhost:11434'),
+        'ollama_model' => env('OLLAMA_MODEL', 'phi3'),
     ],
 
 ];
